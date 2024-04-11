719c908c87bdbd096ebab83c553da893c3a82ebd
Antonio Salinas García <antonio.salinas@ext.cesurformacion.com>
2024-04-11 10:52:38 +0300

    Agregar archivos de proyecto.

---
+++ chatPrueba/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,68 @@
+﻿using chatPrueba.Net;
+using chatPrueba.MVVM.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using chatPrueba.MVVM.Model;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace chatPrueba.MVVM.ViewModel
+{
+    class MainViewModel
+    {
+        public RelayCommand ConnectToServerCommand { get; set; }
+        public RelayCommand SendMessageCommand { get; set; }
+
+        private Server _server;
+        public String Username { get; set; }
+        public String Message { get; set; }
+        public ObservableCollection<UserModel> Users { get; set; } 
+        public ObservableCollection<string> Messages { get; set; }
+        
+        public MainViewModel()
+        {
+            _server = new Server();
+            _server.connectedEvent += UserConnected;
+            _server.msgReceivedEvent += MessageReceived;
+            _server.disconnectedEvent += UserConnected;
+            Users = new ObservableCollection<UserModel>();
+            Messages = new ObservableCollection<string>();
+            ConnectToServerCommand = new RelayCommand(o => _server.ConnectToServer(Username), o => !string.IsNullOrEmpty(Username));
+            SendMessageCommand = new RelayCommand(o => _server.SendMessageToServer(Message), o => !string.IsNullOrEmpty(Message));
+        }
+
+        private void MessageReceived()
+        {
+            var msg = _server.PacketReader.ReadMessage();
+            Application.Current.Dispatcher.Invoke(() => Messages.Add(msg));
+
+        }
+
+        //This will be used whenever we receive an '0' opcode
+        private void UserConnected()
+        {
+            var user = new UserModel
+            {
+                UserName = _server.PacketReader.ReadMessage(),
+                UID = _server.PacketReader.ReadMessage(),
+            };
+
+            if (!Users.Any(x => x.UID == user.UID))
+            { 
+                Application.Current.Dispatcher.Invoke(() => Users.Add(user));
+            }
+
+        }
+
+        private void RemoveUser()
+        {
+            var uid = _server.PacketReader.ReadMessage();
+            var user = Users.Where(x => x.UID == uid).FirstOrDefault();
+            Application.Current.Dispatcher.Invoke(() => Users.Remove(user));
+        }
+    }
+}
---
+++ chatPrueba/Net/Server.cs
@@ -0,0 +1,74 @@
+﻿using chatPrueba.Net.IO;
+using System.Net.Sockets;
+
+namespace chatPrueba.Net
+{
+    class Server
+    {
+        TcpClient _client;
+        public PacketReader PacketReader;
+
+        public event Action connectedEvent;
+        public event Action msgReceivedEvent;
+        public event Action disconnectedEvent;
+
+        public Server()
+        {
+            _client = new TcpClient();
+        }
+
+        public void ConnectToServer(String username)
+        {
+
+            if (!_client.Connected)
+            {
+                _client.Connect("127.0.0.1", 7891);
+                PacketReader = new PacketReader(_client.GetStream());
+                if (!string.IsNullOrEmpty(username))
+                {
+                    var connectPacket = new PacketBuilder();
+                    connectPacket.WriteOpCode(0);
+                    connectPacket.WriteString(username);
+                    _client.Client.Send(connectPacket.GetPacketBytes());
+                }
+                ReadPackets();
+
+            }
+        }
+
+        private void ReadPackets()
+        {
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    // TODO : differ between opcodes, by doing that function we could send files, images and all of that
+
+                    var opcode = PacketReader.ReadByte();
+                    switch (opcode)
+                    {
+                        case 1:
+                            connectedEvent?.Invoke();
+                            break;
+                        case 2:
+                            msgReceivedEvent?.Invoke();
+                            break;
+                        case 3:
+                            disconnectedEvent?.Invoke();
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            });
+        }
+
+        public void SendMessageToServer(String messsage)
+        {
+            var messagePacket = new PacketBuilder();
+            messagePacket.WriteOpCode(2);
+            messagePacket.WriteString(messsage);
+            _client.Client.Send(messagePacket.GetPacketBytes());
+        }
+    }
+}
